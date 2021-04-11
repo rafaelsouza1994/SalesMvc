e1d2f75342b605284cce159af74be8b964974e18
Rafael Menezes <rafaelsouza4520@gmail.com>
2021-04-11 20:18:25 -0300

    Eager Loading in Products

--- SalesWebMvc/Services/ProductService.cs
+++ SalesWebMvc/Services/ProductService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -28,7 +29,7 @@ namespace SalesWebMvc.Services
 
         public Product FindById(int id)
         {
-            return _context.Product.FirstOrDefault(obj => obj.Id == id);
+            return _context.Product.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
